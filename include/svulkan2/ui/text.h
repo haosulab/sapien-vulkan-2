3f49571064f609f0e8921ab6dc5ea9c1fc6584d6
Fanbo Xiang <xiangfanbo@gmail.com>
2021-03-14 00:57:03 -0800

    progress on ui

--- include/svulkan2/ui/text.h
+++ include/svulkan2/ui/text.h
@@ -1,18 +1,22 @@
 #pragma once
 #include "svulkan2/common/glm.h"
 #include "widget.h"
+#include <functional>
 
 namespace svulkan2 {
 namespace ui {
 
 UI_CLASS(DisplayText) {
   UI_ATTRIBUTE(DisplayText, std::string, Text);
+
   void build() override;
 };
 
 UI_CLASS(InputText) {
   UI_ATTRIBUTE(InputText, std::string, Label);
   UI_ATTRIBUTE(InputText, bool, ReadOnly);
+  UI_ATTRIBUTE(InputText, std::function<void(std::shared_ptr<InputText>)>,
+               Callback);
 
 protected:
   std::vector<char> mBuffer{std::vector<char>(100, 0)};
@@ -32,6 +36,8 @@ UI_CLASS(InputFloat) {
   UI_ATTRIBUTE(InputFloat, std::string, Label);
   UI_ATTRIBUTE(InputFloat, float, Value);
   UI_ATTRIBUTE(InputText, bool, ReadOnly);
+  UI_ATTRIBUTE(InputText, std::function<void(std::shared_ptr<InputFloat>)>,
+               Callback);
 
 public:
   inline float get() const { return mValue; }
@@ -42,6 +48,8 @@ UI_CLASS(InputFloat2) {
   UI_ATTRIBUTE(InputFloat2, std::string, Label);
   UI_ATTRIBUTE(InputFloat, glm::vec2, Value);
   UI_ATTRIBUTE(InputText, bool, ReadOnly);
+  UI_ATTRIBUTE(InputText, std::function<void(std::shared_ptr<InputFloat2>)>,
+               Callback);
 
 public:
   inline glm::vec2 get() const { return mValue; }
@@ -52,6 +60,8 @@ UI_CLASS(InputFloat3) {
   UI_ATTRIBUTE(InputFloat3, std::string, Label);
   UI_ATTRIBUTE(InputFloat, glm::vec3, Value);
   UI_ATTRIBUTE(InputText, bool, ReadOnly);
+  UI_ATTRIBUTE(InputText, std::function<void(std::shared_ptr<InputFloat3>)>,
+               Callback);
 
 public:
   inline glm::vec3 get() const { return mValue; }
@@ -62,6 +72,8 @@ UI_CLASS(InputFloat4) {
   UI_ATTRIBUTE(InputFloat4, std::string, Label);
   UI_ATTRIBUTE(InputFloat, glm::vec4, Value);
   UI_ATTRIBUTE(InputText, bool, ReadOnly);
+  UI_ATTRIBUTE(InputText, std::function<void(std::shared_ptr<InputFloat4>)>,
+               Callback);
 
 public:
   inline glm::vec4 get() const { return mValue; }
--- src/ui/text.cpp
+++ src/ui/text.cpp
@@ -6,21 +6,44 @@ namespace ui {
 void DisplayText::build() { ImGui::Text("%s", mText.c_str()); }
 void InputText::build() {
   ImGuiInputTextFlags flags = mReadOnly ? ImGuiInputTextFlags_ReadOnly : 0;
-  ImGui::InputText(mLabel.c_str(), mBuffer.data(), mBuffer.size(), flags);
+  flags |= ImGuiInputTextFlags_EnterReturnsTrue;
+  if (ImGui::InputText(mLabel.c_str(), mBuffer.data(), mBuffer.size(), flags) &&
+      mCallback) {
+    mCallback(std::static_pointer_cast<InputText>(shared_from_this()));
+  }
 }
 void InputFloat::build() {
   ImGuiInputTextFlags flags = mReadOnly ? ImGuiInputTextFlags_ReadOnly : 0;
-  ImGui::InputFloat(mLabel.c_str(), &mValue, 0.f, 0.f, "%.3f", flags);
+  flags |= ImGuiInputTextFlags_EnterReturnsTrue;
+  if (ImGui::InputFloat(mLabel.c_str(), &mValue, 0.f, 0.f, "%.3f", flags) &&
+      mCallback) {
+    mCallback(std::static_pointer_cast<InputFloat>(shared_from_this()));
+  }
 }
 void InputFloat2::build() {
   ImGuiInputTextFlags flags = mReadOnly ? ImGuiInputTextFlags_ReadOnly : 0;
-  ImGui::InputFloat2(mLabel.c_str(), &mValue[0], "%.3f", flags); }
+  flags |= ImGuiInputTextFlags_EnterReturnsTrue;
+  if (ImGui::InputFloat2(mLabel.c_str(), &mValue[0], "%.3f", flags) &&
+      mCallback) {
+    mCallback(std::static_pointer_cast<InputFloat2>(shared_from_this()));
+  }
+}
 void InputFloat3::build() {
   ImGuiInputTextFlags flags = mReadOnly ? ImGuiInputTextFlags_ReadOnly : 0;
-  ImGui::InputFloat3(mLabel.c_str(), &mValue[0], "%.3f", flags); }
+  flags |= ImGuiInputTextFlags_EnterReturnsTrue;
+  if (ImGui::InputFloat3(mLabel.c_str(), &mValue[0], "%.3f", flags) &&
+      mCallback) {
+    mCallback(std::static_pointer_cast<InputFloat3>(shared_from_this()));
+  }
+}
 void InputFloat4::build() {
   ImGuiInputTextFlags flags = mReadOnly ? ImGuiInputTextFlags_ReadOnly : 0;
-  ImGui::InputFloat4(mLabel.c_str(), &mValue[0], "%.3f", flags); }
+  flags |= ImGuiInputTextFlags_EnterReturnsTrue;
+  if (ImGui::InputFloat4(mLabel.c_str(), &mValue[0], "%.3f", flags) &&
+      mCallback) {
+    mCallback(std::static_pointer_cast<InputFloat4>(shared_from_this()));
+  }
+}
 
 } // namespace ui
 } // namespace svulkan2
