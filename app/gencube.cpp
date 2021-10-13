c6375c721dd5d8551ef5d79eefac30a5a462f3d2
Fanbo Xiang <xiangfanbo@gmail.com>
2021-10-12 19:13:52 -0700

    add program for generating ktx file

---
+++ app/gencube.cpp
@@ -0,0 +1,31 @@
+#include "svulkan2/common/fs.h"
+#include "svulkan2/common/log.h"
+#include "svulkan2/core/context.h"
+#include "svulkan2/renderer/renderer.h"
+#include "svulkan2/scene/scene.h"
+#include "svulkan2/shader/compute.h"
+#include "svulkan2/shader/shader.h"
+#include "svulkan2/ui/ui.h"
+#include <iostream>
+
+using namespace svulkan2;
+
+int main(int argc, char *argv[]) {
+  if (argc != 7) {
+    std::cerr << "gencube takes exactly 6 filenames" << std::endl;
+    return 1;
+  }
+
+  svulkan2::log::getLogger()->set_level(spdlog::level::info);
+
+  auto context = svulkan2::core::Context::Create(true, 5000, 5000, 4);
+  auto manager = context->createResourceManager();
+  auto cubemap = context->getResourceManager()->CreateCubemapFromFiles(
+      {argv[1], argv[2], argv[3], argv[4], argv[5], argv[6]}, 5);
+  cubemap->load();
+  cubemap->uploadToDevice(context);
+
+  cubemap->exportKTX("out.ktx");
+
+  return 0;
+}
