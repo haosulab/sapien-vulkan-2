d17491fe9710fcff356eed082aea8940907ce781
Fanbo Xiang <fxiang@eng.ucsd.edu>
2024-03-03 20:30:49 -0800

    [hack] disable openvr log

--- cmake/openvr.cmake
+++ cmake/openvr.cmake
@@ -12,3 +12,6 @@ if(NOT openvr_POPULATED)
   FetchContent_Populate(openvr)
   add_subdirectory(${openvr_SOURCE_DIR} ${openvr_BINARY_DIR} EXCLUDE_FROM_ALL)
 endif()
+
+# HACK: disable openvr log
+target_compile_options(openvr_api PRIVATE -include ${CMAKE_CURRENT_SOURCE_DIR}/cmake/openvr_definitions.h)
---
+++ cmake/openvr_definitions.h
@@ -0,0 +1,1 @@
+#define VRLog(args...) (void)0
